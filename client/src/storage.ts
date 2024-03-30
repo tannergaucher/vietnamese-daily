972afbc0bae1da60dddb6549d29631b37e7cbf6c
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-03-30 09:49:18 -0400

    use env credentials

--- client/src/storage.ts
+++ client/src/storage.ts
@@ -2,7 +2,11 @@ import { Storage } from "@google-cloud/storage";
 
 export const storage = new Storage({
   projectId: "vietnamese-daily",
-  keyFilename: "service-account.json",
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    project_id: process.env.GCP_PROJECT_ID,
+    private_key: process.env.GCP_PRIVATE_KEY,
+  },
 });
 
 export const dialogAudioBucket = storage.bucket("dialog-audio");
