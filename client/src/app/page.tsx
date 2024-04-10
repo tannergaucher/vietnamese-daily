5d6d680f10914e2bddd4d6f2ae5a0cbe5fd3ec8f
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-04-10 17:52:11 -0400

    handle gcp signed urls

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,14 +2,17 @@ import { prisma } from "../prisma";
 import Link from "next/link";
 import Image from "next/image";
 
+import { conversationImageBucket, getSignedUrl } from "../storage";
+
 export default async function Home() {
-  const conversations = await prisma.conversation.findMany({
+  let conversations = await prisma.conversation.findMany({
     where: {
       published: true,
     },
     include: {
       situation: {
         select: {
+          id: true,
           imageSrc: true,
           text: true,
         },
@@ -17,33 +20,57 @@ export default async function Home() {
     },
   });
 
+  conversations = await Promise.all(
+    conversations.map(async (conversation) => {
+      if (!conversation.situation) {
+        throw new Error("Situation is not found");
+      }
+
+      const signedUrl = await getSignedUrl({
+        filePath: `${conversation.situation.id}.webp`,
+        bucket: conversationImageBucket,
+      });
+      return {
+        ...conversation,
+        situation: { ...conversation.situation, imageSrc: signedUrl },
+      };
+    })
+  );
+
   return (
     <main>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        {conversations.map((conversation) => (
-          <Link href={`conversation/${conversation.id}`} key={conversation.id}>
-            <div className="border rounded-lg p-4 box-border shadow-lg h-auto">
-              {conversation.situation?.imageSrc ? (
-                <Image
-                  src={conversation.situation.imageSrc}
-                  width={1000}
-                  height={1000}
-                  alt="Conversation Image"
-                  className="rounded-lg"
-                />
-              ) : null}
-              <small className="block my-2 text-gray-600 dark:text-gray-300">
-                {conversation.createdAt.toDateString()}
-              </small>
-              <h2 className="text-2xl font-semibold mb-2">
-                {conversation.title}
-              </h2>
-              <p className="text-gray-600 dark:text-gray-300">
-                {conversation.situation?.text}
-              </p>
-            </div>
-          </Link>
-        ))}
+        {conversations.map((conversation) => {
+          console.log(conversation.situation?.imageSrc);
+
+          return (
+            <Link
+              href={`conversation/${conversation.id}`}
+              key={conversation.id}
+            >
+              <div className="border rounded-lg p-4 box-border shadow-lg h-auto">
+                {conversation.situation?.imageSrc ? (
+                  <Image
+                    src={conversation.situation.imageSrc}
+                    width={1000}
+                    height={1000}
+                    alt="Conversation Image"
+                    className="rounded-lg"
+                  />
+                ) : null}
+                <small className="block my-2 text-gray-600 dark:text-gray-300">
+                  {conversation.createdAt.toDateString()}
+                </small>
+                <h2 className="text-2xl font-semibold mb-2">
+                  {conversation.title}
+                </h2>
+                <p className="text-gray-600 dark:text-gray-300">
+                  {conversation.situation?.text}
+                </p>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </main>
   );
