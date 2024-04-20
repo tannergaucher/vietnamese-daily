fe0ef6c7a0420e7bdc433bdab7a99a6957ee3bde
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-04-20 11:41:47 -0400

    Update card styles

--- client/src/app/components/card.tsx
+++ client/src/app/components/card.tsx
@@ -39,36 +39,46 @@ export function Card({
     large: "my-6",
   };
 
+  const marginB = {
+    small: "mb-1",
+    medium: "mb-2",
+    large: "mb-6",
+  };
+
   return (
-    <div className="border border-bg-1-light dark:border-accent-1-dark dark:hover:border-accent-1-dark rounded-lg p-3 box-border shadow-lg h-auto">
+    <div className="border border-bg-1-light dark:border-accent-1-dark dark:hover:border-accent-1-dark rounded-lg box-border shadow-lg h-auto">
       {image ? (
         <Image
           src={image.src}
           width={image.width}
           height={image.height}
           alt={image.alt}
-          className="rounded-lg shadow-lg mb-3"
+          className={`rounded-lg shadow-lg ${marginB[size]}`}
         />
       ) : null}
-      {small ? (
-        <small
-          className={`${fonts[size].small} block ${marginY[size]} text-gray-600 dark:text-gray-300`}
-        >
-          {small}
-        </small>
-      ) : null}
+      <div className="px-3">
+        {small ? (
+          <small
+            className={`${fonts[size].small} block ${marginY[size]} text-gray-600 dark:text-gray-300`}
+          >
+            {small}
+          </small>
+        ) : null}
 
-      <h2 className={`${fonts[size].heading} ${marginY[size]}  font-semibold`}>
-        {heading}
-      </h2>
-      {subHeading ? (
-        <p
-          className={`${fonts[size].subHeading} ${marginY[size]} text-gray-600 dark:text-gray-300`}
+        <h2
+          className={`${fonts[size].heading} ${marginY[size]}  font-semibold`}
         >
-          {subHeading}
-        </p>
-      ) : null}
-      {children}
+          {heading}
+        </h2>
+        {subHeading ? (
+          <p
+            className={`${fonts[size].subHeading} ${marginY[size]} text-gray-600 dark:text-gray-300`}
+          >
+            {subHeading}
+          </p>
+        ) : null}
+        {children}
+      </div>
     </div>
   );
 }
