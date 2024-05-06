de957def3975f744d2f5ac0052bb92cf62f69341
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-05-06 22:27:52 +0700

    Add filter listbox and remove filter btns

--- client/src/app/components/filters.tsx
+++ client/src/app/components/filters.tsx
@@ -1,31 +1,23 @@
-import { Transition } from "@headlessui/react";
+import { Listbox } from "@headlessui/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useState, useEffect, useRef, RefObject, Suspense } from "react";
+import React from "react";
 
-import { Button } from "@/app/components/button";
+const options = [
+  "at the restaurant",
+  "at the cafe",
+  "at the street food vendor stall",
+  "at the market",
+  "asking a local for directions",
+  "a health related situation",
+  "an emergency situation",
+  "at the hotel",
+  "shopping at a store",
+];
 
 export function Filters() {
-  const [isShowing, setIsShowing] = useState(false);
-
-  const ref = useRef(null);
-
-  useOnClickOutside(ref, () => setIsShowing(false));
-
-  const router = useRouter();
-
   const searchParams = useSearchParams();
 
-  const filterOptions = [
-    "at the restaurant",
-    "at the cafe",
-    "at the street food vendor stall",
-    "at the market",
-    "asking a local for directions",
-    "a health related situation",
-    "an emergency situation",
-    "at the hotel",
-    "shopping at a store",
-  ];
+  const router = useRouter();
 
   const typeParams = searchParams.getAll("type");
 
@@ -39,6 +31,7 @@ export function Filters() {
 
       if (newTypeParams.length === 0) {
         router.push("/");
+
         return;
       }
 
@@ -50,75 +43,60 @@ export function Filters() {
     router.push(`?type=${[...typeParams, encodedFilterOption].join("&type=")}`);
   };
 
+  const isActive = (option: string) => {
+    return typeParams.includes(option);
+  };
+
   return (
-    <Suspense>
-      <Button
-        onClick={() => setIsShowing((isShowing) => !isShowing)}
-        className="w-fit justify-self-end"
-        secondary
-      >
-        Filter
-      </Button>
-      <Transition
-        show={isShowing}
-        enter="transition-opacity transition-transform duration-200 ease-in-out"
-        enterFrom="opacity-0 transform translate-x-[100%]"
-        enterTo="opacity-100 transform translate-x-0"
-        leave="transition-opacity transition-transform duration-200 ease-in-out"
-        leaveFrom="opacity-100 transform translate-x-0"
-        leaveTo="opacity-0 transform translate-x-[100%]"
-      >
-        <div
-          className="absolute right-0 bg-bg-1-light dark:bg-bg-1-dark rounded p-2"
-          ref={ref}
-        >
-          <ul>
-            <li>
-              <Button
-                disabled={typeParams.length === 0}
-                className="w-full my-2"
-                onClick={() => router.push("/")}
+    <div className="relative w-full">
+      <Listbox onChange={(value) => handleFilterClick(value)}>
+        <div className="relative mt-1">
+          <Listbox.Button className="absolute top-1/2 transform -translate-y-1/2 right-0 h-12 py-2 pl-3 pr-10 text-left text-lg font-bold dark:bg-accent-2-dark rounded-lg border border-gray-300 cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            <span className="block truncate">Situations</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="none"
+                stroke="currentColor"
               >
-                Clear filters
-              </Button>
-            </li>
-            {filterOptions.map((filterOption) => (
-              <li key={filterOption}>
-                <Button
-                  className="w-full my-2"
-                  secondary
-                  selected={typeParams.includes(filterOption)}
-                  onClick={() => handleFilterClick(filterOption)}
-                >
-                  {filterOption}
-                </Button>
-              </li>
+                <path
+                  d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </span>
+          </Listbox.Button>
+          <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-bg-1-light dark:bg-bg-1-dark rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {options.map((option) => (
+              <Listbox.Option
+                key={option}
+                value={option}
+                className={`${
+                  isActive(option)
+                    ? "text-amber-900 bg-amber-100"
+                    : "text-gray-900 dark:text-text-color-dark"
+                }
+                        cursor-default select-none hover:accent-2-light dark:hover:bg-accent-2-dark relative py-2 pl-10 pr-4`}
+              >
+                {() => (
+                  <>
+                    <span
+                      className={`${
+                        isActive(option) ? "font-medium" : "font-normal"
+                      } block truncate`}
+                    >
+                      {option}
+                    </span>
+                  </>
+                )}
+              </Listbox.Option>
             ))}
-          </ul>
+          </Listbox.Options>
         </div>
-      </Transition>
-    </Suspense>
+      </Listbox>
+    </div>
   );
-}
-
-function useOnClickOutside(
-  ref: RefObject<HTMLElement>,
-  handler: (event: MouseEvent | TouchEvent) => void
-) {
-  useEffect(() => {
-    const listener = (event: MouseEvent | TouchEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        return;
-      }
-      handler(event);
-    };
-
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
-
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [ref, handler]);
 }
