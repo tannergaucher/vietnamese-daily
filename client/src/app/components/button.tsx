3ce6c6700a50f193408971a08b60e9fc53348f70
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-13 16:18:00 +0700

    refactor: Update button component styles for consistency and clarity

--- client/src/app/components/button.tsx
+++ client/src/app/components/button.tsx
@@ -4,7 +4,7 @@ export const PRIMARY =
   "transition-all duration-200 ease-in-out bg-accent-2-light hover:bg-accent-1-light focus:bg-accent-1-light text-accent-1-light hover:text-accent-2-light focus:text-accent-2-light dark:bg-accent-2-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:text-accent-1-dark dark:hover:text-accent-2-dark dark:focus:text-accent-2-dark dark:border-accent-2-dark";
 
 export const SECONDARY =
-  "transition-all duration-200 ease-in-out bg-bg-1-light border border-bg-2-light text-accent-2-light hover:bg-bg-2-light focus:bg-bg-2-light hover:text-bg-1-light focus:text-bg-1-light dark:bg-bg-1-dark dark:border dark:border-accent-1-dark  dark:text-accent-1-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:hover:text-bg-1-dark dark:focus:text-bg-1-dark";
+  "transition-all duration-200 ease-in-out bg-bg-1-light border-2 border-bg-2-light text-accent-2-light hover:bg-bg-2-light focus:bg-bg-2-light hover:text-bg-1-light focus:text-bg-1-light dark:bg-bg-1-dark dark:border-2 dark:border-accent-1-dark  dark:text-accent-1-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:hover:text-bg-1-dark dark:focus:text-bg-1-dark";
 
 export function Button({
   onClick,
