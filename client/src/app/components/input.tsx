cdb005a0843f85c76449640bf6f090e2b4838432
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-13 15:47:05 +0700

    feat: Add hover effect to input component

--- client/src/app/components/input.tsx
+++ client/src/app/components/input.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const INPUT_CLASSES =
-  "my-4 p-2 rounded h-12 dark:bg-bg-2-light dark:text-accent-1-dark dark:border-accent-2-dark border-none shadow-inner focus:outline-none focus:ring-2 focus:ring-accent-2-light focus:ring-opacity-50";
+  "my-4 p-2 rounded h-12 dark:bg-bg-2-light dark:text-accent-1-dark dark:border-accent-2-dark border-none shadow-inner focus:outline-none focus:ring-2 focus:ring-accent-2-light focus:ring-opacity-50 hover:ring-2 hover:ring-accent-2-light hover:ring-opacity-50";
 
 export function Input({
   value,
