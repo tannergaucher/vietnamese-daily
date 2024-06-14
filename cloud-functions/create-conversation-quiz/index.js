9106fa866b9b8225d113a2e49808038d1f538e2d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-14 15:57:39 +0700

    Init openai assistant

--- cloud-functions/create-conversation-quiz/index.js
+++ cloud-functions/create-conversation-quiz/index.js
@@ -31,40 +31,96 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createConversationQuiz = void 0;
+const fs_1 = __importDefault(require("fs"));
 const cloud_function_events_1 = require("@functional-vietnamese/cloud-function-events");
 const functions = __importStar(require("@google-cloud/functions-framework"));
+const openai_1 = __importDefault(require("openai"));
 const generated_1 = require("./generated");
 functions.cloudEvent("createConversationQuiz", (cloudEvent) => __awaiter(void 0, void 0, void 0, function* () {
-    const { conversationId } = (0, cloud_function_events_1.parseCloudEventData)({
+    const { conversationId, assistantId } = (0, cloud_function_events_1.parseCloudEventData)({
         cloudEvent,
     });
     const prisma = new generated_1.PrismaClient();
+    const openai = new openai_1.default({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
     const response = yield createConversationQuiz({
         conversationId,
+        assistantId,
         prisma,
+        openai,
     });
     return response;
 }));
 function createConversationQuiz(_a) {
-    return __awaiter(this, arguments, void 0, function* ({ conversationId, prisma, }) {
-        // query the conversation by id
-        const conversation = yield prisma.conversation.findUnique({
-            where: {
-                id: conversationId,
-            },
-            select: {
-                title: true,
-                situation: true,
-                dialog: true,
-            },
-        });
-        console.log(conversation);
-        // take that conversation, write to a json file
-        // attach that json file to the chatgpt assistant conversation
-        // prompt the assitant to create a quiz based on the conversation content
-        // save the quiz to the database
+    return __awaiter(this, arguments, void 0, function* ({ conversationId, prisma, openai, }) {
+        // const conversation = await prisma.conversation.findUniqueOrThrow({
+        //   where: {
+        //     id: conversationId,
+        //   },
+        //   select: {
+        //     title: true,
+        //     situation: true,
+        //     dialog: true,
+        //   },
+        // });
+        // // Write conversation data to a local JSON file
+        // const conversationData = JSON.stringify(conversation);
+        // const filePath = "./conversation.json";
+        // fs.writeFileSync(filePath, conversationData);
+        // const file = await openai.files.create({
+        //   file: fs.createReadStream(filePath),
+        //   purpose: "assistants",
+        // });
+        // console.log(file, "file");
+        // const assistant = await openai.beta.assistants.create({
+        //   name: "Vietnamese Conversation Quiz Assistant",
+        //   instructions: "Create a quiz based on a Vietnamese conversation",
+        //   model: "gpt-3.5-turbo",
+        //   tools: [
+        //     {
+        //       type: "file_search",
+        //     },
+        //   ],
+        // });
+        // console.log(assistant, "assistant");
+        // const thread = await openai.beta.threads.create({
+        //   messages: [
+        //     {
+        //       role: "assistant",
+        //       content:
+        //         "You are creating content for a language learning application. You have a conversation between two people in Vietnamese. You want to create a quiz based on the conversation. The conversation is attached here as a file. Please create a quiz based on the conversation.",
+        //       attachments: [
+        //         {
+        //           file_id: file.id,
+        //           tools: [{ type: "file_search" }],
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // });
+        // console.log(thread, "thread");
+        // // now run the thread
+        // const completion = await openai.beta.threads.runs.create(thread.id, {
+        //   model: "gpt-3.5-turbo",
+        //   assistant_id: assistant.id,
+        // });
+        // heres the thread run id: run_gz8CMYwp4Akvp5vT2HQ3JOqd
+        // how to get the data of the run?
+        // get the run data
+        const threadMessagesList = yield openai.beta.threads.messages.list("thread_JbdCs5CRCrerznESs5UMBREX");
+        const quizData = threadMessagesList.data
+            .map((message) => message.content)
+            .map((content) => JSON.stringify(content[0].text, null, 2));
+        const parsedQuizData = quizData.map((data) => JSON.parse(data));
+        const formattedQuizData = parsedQuizData.map((data) => JSON.stringify(data, null, 2).replace(/\\n/g, " "));
+        const quizFilePath = "./quiz.json";
+        fs_1.default.writeFileSync(quizFilePath, JSON.stringify(formattedQuizData, null, 2));
     });
 }
 exports.createConversationQuiz = createConversationQuiz;
--- cloud-functions/create-conversation-quiz/index.ts
+++ cloud-functions/create-conversation-quiz/index.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 import {
   CloudEventData,
   CreateConversationQuizEvent,
@@ -5,6 +7,7 @@ import {
 } from "@functional-vietnamese/cloud-function-events";
 import * as functions from "@google-cloud/functions-framework";
 import OpenAi from "openai";
+import { TextContentBlock } from "openai/resources/beta/threads/messages";
 
 import { PrismaClient } from "./generated";
 
@@ -40,27 +43,87 @@ functions.cloudEvent(
 
 export async function createConversationQuiz({
   conversationId,
-  assistantId,
   prisma,
   openai,
 }: CreateConversationQuizParams) {
-  // query the conversation by id
-  const conversation = await prisma.conversation.findUniqueOrThrow({
-    where: {
-      id: conversationId,
-    },
-    select: {
-      title: true,
-      situation: true,
-      dialog: true,
-    },
-  });
+  // const conversation = await prisma.conversation.findUniqueOrThrow({
+  //   where: {
+  //     id: conversationId,
+  //   },
+  //   select: {
+  //     title: true,
+  //     situation: true,
+  //     dialog: true,
+  //   },
+  // });
 
-  // take that conversation, write to a json file
-  // attach that json file to the chatgpt assistant conversation
+  // // Write conversation data to a local JSON file
+  // const conversationData = JSON.stringify(conversation);
+  // const filePath = "./conversation.json";
+  // fs.writeFileSync(filePath, conversationData);
 
-  const assistant = await openai.beta.assistants.retrieve(assistantId);
+  // const file = await openai.files.create({
+  //   file: fs.createReadStream(filePath),
+  //   purpose: "assistants",
+  // });
 
-  // prompt the assitant to create a quiz based on the conversation content
-  // save the quiz to the database
+  // console.log(file, "file");
+
+  // const assistant = await openai.beta.assistants.create({
+  //   name: "Vietnamese Conversation Quiz Assistant",
+  //   instructions: "Create a quiz based on a Vietnamese conversation",
+  //   model: "gpt-3.5-turbo",
+  //   tools: [
+  //     {
+  //       type: "file_search",
+  //     },
+  //   ],
+  // });
+
+  // console.log(assistant, "assistant");
+
+  // const thread = await openai.beta.threads.create({
+  //   messages: [
+  //     {
+  //       role: "assistant",
+  //       content:
+  //         "You are creating content for a language learning application. You have a conversation between two people in Vietnamese. You want to create a quiz based on the conversation. The conversation is attached here as a file. Please create a quiz based on the conversation.",
+  //       attachments: [
+  //         {
+  //           file_id: file.id,
+  //           tools: [{ type: "file_search" }],
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // });
+
+  // console.log(thread, "thread");
+
+  // // now run the thread
+  // const completion = await openai.beta.threads.runs.create(thread.id, {
+  //   model: "gpt-3.5-turbo",
+  //   assistant_id: assistant.id,
+  // });
+
+  // heres the thread run id: run_gz8CMYwp4Akvp5vT2HQ3JOqd
+  // how to get the data of the run?
+  // get the run data
+
+  const threadMessagesList = await openai.beta.threads.messages.list(
+    "thread_JbdCs5CRCrerznESs5UMBREX"
+  );
+
+  const quizData = threadMessagesList.data
+    .map((message) => message.content as TextContentBlock[])
+    .map((content) => JSON.stringify(content[0].text, null, 2));
+
+  const parsedQuizData = quizData.map((data: string) => JSON.parse(data));
+  const formattedQuizData = parsedQuizData.map((data: object) =>
+    JSON.stringify(data, null, 2).replace(/\\n/g, " ")
+  );
+
+  const quizFilePath = "./quiz.json";
+
+  fs.writeFileSync(quizFilePath, JSON.stringify(formattedQuizData, null, 2));
 }
