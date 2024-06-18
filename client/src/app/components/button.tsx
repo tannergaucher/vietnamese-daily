73a4c9a36602e63fd9d0b776bfaa3fb1924324e1
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-18 16:15:04 +0700

    Refactor Button component to use shared styles for primary and secondary buttons

--- client/src/app/components/button.tsx
+++ client/src/app/components/button.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export const PRIMARY =
-  "transition-all duration-200 ease-in-out bg-accent-2-light hover:bg-accent-1-light focus:bg-accent-1-light text-accent-1-light hover:text-accent-2-light focus:text-accent-2-light dark:bg-accent-2-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:text-accent-1-dark dark:hover:text-accent-2-dark dark:focus:text-accent-2-dark dark:border-accent-2-dark rounded p-3 shadow h-12 text-lg font-semibold";
+export const SHARED =
+  "transition-all duration-200 ease-in-out rounded p-3 shadow h-12 text-lg font-semibold";
 
-export const SECONDARY =
-  "transition-all duration-200 ease-in-out bg-bg-1-light border-2 border-bg-2-light text-accent-2-light hover:bg-bg-2-light focus:bg-bg-2-light hover:text-bg-1-light focus:text-bg-1-light dark:bg-bg-1-dark dark:border-2 dark:border-accent-1-dark  dark:text-accent-1-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:hover:text-bg-1-dark dark:focus:text-bg-1-dark rounded p-3 shadow h-12 text-lg font-semibold";
+export const PRIMARY = `${SHARED} bg-accent-2-light hover:bg-accent-1-light focus:bg-accent-1-light text-accent-1-light hover:text-accent-2-light focus:text-accent-2-light dark:bg-accent-2-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:text-accent-1-dark dark:hover:text-accent-2-dark dark:focus:text-accent-2-dark dark:border-accent-2-dark`;
+
+export const SECONDARY = `${SHARED} bg-bg-1-light border-2 border-bg-2-light text-accent-2-light hover:bg-bg-2-light focus:bg-bg-2-light hover:text-bg-1-light focus:text-bg-1-light dark:bg-bg-1-dark dark:border-2 dark:border-accent-1-dark  dark:text-accent-1-dark dark:hover:bg-accent-1-dark dark:focus:bg-accent-1-dark dark:hover:text-bg-1-dark dark:focus:text-bg-1-dark`;
 
 export function Button({
   onClick,
