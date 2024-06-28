cad3cb406b9cda80fc0aa893dada30170e9b426f
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-28 13:07:34 +0700

    chore: Update container component to include horizontal padding

--- client/src/app/components/container.tsx
+++ client/src/app/components/container.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Container({ children }: { children: React.ReactNode }) {
-  return <div className="container mx-auto">{children}</div>;
+  return <div className="container mx-auto px-2">{children}</div>;
 }
